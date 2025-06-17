aa362acea56a88357897a84d48a706da9dc76425
Forge <28455546+forge34@users.noreply.github.com>
2025-06-17 20:23:45 +0300

    refactor: optimized get all users function

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,5 +1,5 @@
 import { Conversation, User } from "@prisma/client";
-import {  Request, Response } from "express";
+import { Request, Response } from "express";
 import passport from "passport";
 import { prisma } from "../config/prisma-client";
 import expressAsyncHandler from "express-async-handler";
@@ -11,64 +11,51 @@ interface IUser extends Omit<User, "password"> {
 
 class UserController {
   static getCurrent = [
-    passport.authenticate("jwt", { session: false,failWithError:true }),
+    passport.authenticate("jwt", { session: false, failWithError: true }),
     expressAsyncHandler(async (req: Request, res: Response) => {
       res.status(200).json(req.user as User);
     }),
   ];
 
   static getMany = [
-    passport.authenticate("jwt", { session: false,failWithError:true }),
+    passport.authenticate("jwt", { session: false, failWithError: true }),
     async (req: Request, res: Response) => {
-      const users = await prisma.user.findMany({});
       const currentUser = req.user as User;
-      const filteredUsers: IUser[] = [];
+      const users = await prisma.user.findMany({
+        select: {
+          id: true,
+          imgUrl: true,
+          name: true,
+        },
+      });
 
-      for (let user of users) {
-        if (user.id === currentUser.id) continue;
+      const conversations = await prisma.conversation.findMany({
+        include: {
+          users: true,
+        },
+      });
 
-        const privateConversation = await prisma.conversation.findFirst({
-          where: {
-            AND: [
-              {
-                users: {
-                  some: {
-                    id: currentUser.id,
-                  },
-                },
-              },
-              {
-                users: {
-                  some: {
-                    id: user.id,
-                  },
-                },
-              },
-            ],
-          },
+      const mappedUsers = users.map((u) => {
+        const conversation = conversations.find((c) => {
+          return (
+            c.users.some((u1) => u1.id === u.id) &&
+            c.users.some((u2) => u2.id === currentUser.id)
+          );
         });
 
-        const { password, ...rest } = user;
-        if (privateConversation) {
-          filteredUsers.push({
-            ...rest,
-            privateConversation,
-            relatedToCurrent: true,
-          });
-        } else {
-          filteredUsers.push({
-            ...rest,
-            privateConversation: null,
-            relatedToCurrent: false,
-          });
-        }
-      }
-      res.status(200).json(filteredUsers);
+        return {
+          ...u,
+          isCurrent: u.id === currentUser.id,
+          conversationId: conversation?.id ?? null,
+        };
+      });
+
+      res.status(200).json(mappedUsers);
     },
   ];
 
   static blockUser = [
-    passport.authenticate("jwt", { session: false,failWithError:true }),
+    passport.authenticate("jwt", { session: false, failWithError: true }),
     expressAsyncHandler(async (req: Request, res: Response) => {
       const user = req.user as User;
       const blockedId = req.params.userid as string;
