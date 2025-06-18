ad624cd8207fddd5be959b1507aebdd50680cfe7
Forge <28455546+forge34@users.noreply.github.com>
2025-06-18 14:22:35 +0300

    refactor: clear query cache on logout

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import { queryClient } from "../router";
 
 export default function Logout() {
   const mutation = useMutation({
@@ -8,8 +9,11 @@ export default function Logout() {
       await fetch(`${import.meta.env.VITE_API}/logout`, {
         mode: "cors",
         method: "POST",
-        credentials:"include"
+        credentials: "include",
       });
+
+      queryClient.clear();
+      queryClient.removeQueries({});
     },
   });
 
