b870adfda35b9f109c2dce92c375b57acac5a95d
Forge <28455546+forge34@users.noreply.github.com>
2025-06-21 22:29:06 +0300

    fix: fix bug with undefined cookie package

--- src/sockets.ts
+++ src/sockets.ts
@@ -1,10 +1,10 @@
 import passport from "passport";
-import cookie from "cookie"
+import  {parse}from "cookie"
 
 export const socketJwtVerify = (req: any, res: any, next: any) => {
   if (!req.headers.cookie) return;
   req.cookies = {};
-  req.cookies.jwt = cookie.parse(req.headers.cookie).jwt;
+  req.cookies.jwt = parse(req.headers.cookie).jwt;
   const isHandshake = req._query.sid === undefined;
   if (isHandshake) {
     passport.authenticate("jwt", { session: false,failWithError:true })(req, res, next);
