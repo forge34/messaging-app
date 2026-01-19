a9805cb088d438ee2d602bdde621f886dc483a97
Forge <28455546+forge34@users.noreply.github.com>
2026-01-19 21:35:14 +0200

    refactor: remove redudant type cast

--- apps/server/src/lib/validate.ts
+++ apps/server/src/lib/validate.ts
@@ -6,7 +6,7 @@ interface TypedRequest<T> extends Request {
   body: T;
 }
 
-interface TypedResponse<T, TData> extends Response {
+interface TypedResponse<TData> extends Response {
   json: ({ message, data }: { message: string; data?: TData | null }) => this;
 }
 
@@ -14,7 +14,7 @@ export function createHandler<T extends ApiRoute>(
   route: T,
   handler: (
     req: TypedRequest<z.infer<T["request"]>>,
-    res: TypedResponse<z.infer<T["response"]>, z.infer<T["response"]>["data"]>,
+    res: TypedResponse<z.infer<T["response"]>["data"]>,
   ) => Promise<void>,
 ) {
   return async function (
@@ -23,7 +23,7 @@ export function createHandler<T extends ApiRoute>(
     next: NextFunction,
   ) {
     const data = route.request.parse(req.body);
-    req.body = data as typeof data;
+    req.body = data;
 
     await handler(req, res);
   };
