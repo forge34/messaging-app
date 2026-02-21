cf17fa90d605873e4cc8544cfde3e674b1f449fb
Forge <28455546+forge34@users.noreply.github.com>
2026-02-21 13:24:26 +0200

    fix: socket handler unused paramters

--- apps/client/src/lib/sockets/handlers.ts
+++ apps/client/src/lib/sockets/handlers.ts
@@ -2,14 +2,22 @@ import { queryClient } from "@/main";
 import type { PublicMessageSchema } from "@chat/shared";
 import { toast } from "sonner";
 
-export async function onMessageCreate(message : PublicMessageSchema, conversationId: string) {
-  toast.info("Messae recieved from " + message.author.name)
+export async function onMessageCreate(
+  message: PublicMessageSchema,
+  conversationId: string,
+) {
+  toast.info("Messae recieved from " + message.author.name);
   queryClient.invalidateQueries({
     queryKey: ["conversations", conversationId],
   });
 }
 
-export async function onMessageConfirm(message, conversationId : string, tempId : string) {
+export async function onMessageConfirm(
+  message: PublicMessageSchema,
+  conversationId: string,
+  tempId: string,
+) {
+  console.log(message, tempId);
   queryClient.invalidateQueries({
     queryKey: ["conversations", conversationId],
   });
