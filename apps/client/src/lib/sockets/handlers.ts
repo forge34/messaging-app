a764a23e837120d158f1ac11b641ed900889830b
Forge <28455546+forge34@users.noreply.github.com>
2026-02-22 21:49:58 +0200

    refactor: invalidate all conversation quries on message

--- apps/client/src/lib/sockets/handlers.ts
+++ apps/client/src/lib/sockets/handlers.ts
@@ -2,23 +2,20 @@ import { queryClient } from "@/main";
 import type { PublicMessageSchema } from "@chat/shared";
 import { toast } from "sonner";
 
-export async function onMessageCreate(
-  message: PublicMessageSchema,
-  conversationId: string,
-) {
-  toast.info("Messae recieved from " + message.author.name);
+export async function onMessageCreate(message: PublicMessageSchema) {
+  toast.info("Message recieved from " + message.author.name);
   queryClient.invalidateQueries({
-    queryKey: ["conversations", conversationId],
+    queryKey: ["conversations"],
   });
 }
 
 export async function onMessageConfirm(
   message: PublicMessageSchema,
-  conversationId: string,
+  _: string,
   tempId: string,
 ) {
   console.log(message, tempId);
   queryClient.invalidateQueries({
-    queryKey: ["conversations", conversationId],
+    queryKey: ["conversations"],
   });
 }
