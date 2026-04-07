6ce8175bddc405cad795cfbdd2f0c21f7a8ec27e
Forge <28455546+forge34@users.noreply.github.com>
2026-04-07 13:17:10 +0200

    feat: add user controller

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -1,11 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Route } from '../common/route.decorator';
+import { Routes } from '@chat/shared';
+import { type Request } from 'express';
+import { User } from '@chat/db/client';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { type RequestParams } from '../common/helpers';
 
-@Controller('users')
+@Controller('')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
-  @Get('')
-  async getAllUsers() {
-    return this.userService.users();
+
+  @UseGuards(JwtAuthGuard)
+  @Route(Routes.getUsers)
+  async getAllUsers(@Req() req: Request) {
+    const user = req.user as User;
+    if (!user) return;
+
+    const data = await this.userService.users(user.id);
+    return { data };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Route(Routes.getCurrentUser)
+  getCurrent(@Req() req: Request) {
+    const user = req.user as User;
+
+    return { data: user };
+  }
+  @UseGuards(JwtAuthGuard)
+  @Route(Routes.getBookmarks)
+  async getBookmarks(@Req() req: Request) {
+    const user = req.user as User;
+    const bookmarks = await this.userService.getBookmarks(user.id);
+    return { data: bookmarks };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Route(Routes.getUserById)
+  async getUserByid(@Param() params: RequestParams<typeof Routes.getUserById>) {
+    const { id } = params;
+
+    const user = await this.userService.user(id);
+
+    return { data: user };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Route(Routes.blockUser)
+  async blockUser(
+    @Req() req: Request,
+    @Param() params: RequestParams<typeof Routes.getUserById>,
+  ) {
+    const user = req.user as User;
+    const { id } = params;
+
+    await this.userService.block(user.id, id);
   }
 }
--- apps/server/src/users/users.service.ts
+++ apps/server/src/users/users.service.ts
@@ -3,18 +3,42 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly Prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  async users() {
-    return this.Prisma.user.findMany({
+  async users(currentUserId: string) {
+    const users = await this.prisma.user.findMany({
       omit: {
         password: true,
       },
     });
+
+    const conversations = await this.prisma.conversation.findMany({
+      include: {
+        users: true,
+      },
+    });
+
+    const mappedUsers = users.map((u) => {
+      const conversation = conversations.find((c) => {
+        return (
+          c.users.some((u1) => u1.id === u.id) &&
+          c.users.some((u2) => u2.id === currentUserId)
+        );
+      });
+
+      return {
+        ...u,
+        isCurrent: u.id === currentUserId,
+        hasConversation: !!conversation?.id,
+        mutualConversation: conversation?.id,
+      };
+    });
+
+    return mappedUsers;
   }
 
   async user(id: string) {
-    return this.Prisma.user.findFirst({
+    return this.prisma.user.findFirst({
       where: {
         id: id,
       },
@@ -31,6 +55,64 @@ export class UsersService {
         },
         bookmarks: true,
         conversations: true,
+      },
+    });
+  }
+
+  async getBookmarks(id: string) {
+    const user = await this.prisma.user.findFirst({
+      select: {
+        id: true,
+        imgUrl: true,
+        name: true,
+        bio: true,
+        bookmarks: {
+          include: {
+            author: {
+              select: {
+                id: true,
+                imgUrl: true,
+                name: true,
+                bio: true,
+              },
+            },
+          },
+        },
+      },
+
+      where: {
+        id: id,
+      },
+    });
+
+    if (!user) return [];
+
+    const bookmarks = user.bookmarks.map(
+      ({ author, id, body, createdAt, status, conversationId, authorId }) => {
+        const isMine = author.id === id;
+        return {
+          id,
+          body,
+          author,
+          createdAt,
+          isMine,
+          isBookmarked: true,
+          status,
+          conversationId,
+          authorId,
+        };
+      },
+    );
+    return bookmarks;
+  }
+
+  async block(id: string, blockedId: string) {
+    await this.prisma.user.update({
+      where: { id: id },
+      data: {
+        blocked: {
+          connect: [{ id: blockedId }],
+        },
       },
     });
   }
